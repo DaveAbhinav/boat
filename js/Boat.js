60bfe5758da3c79c9775d6a5189ccb2e2e8280fc
DaveAbhinav <86666382+DaveAbhinav@users.noreply.github.com>
2021-08-11 18:53:58 +0530

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,30 @@
+class Boat{
+    constructor(x,y,width,height,boatPos){
+        var options = {
+            restitution:1,
+            friction:1,
+            density:1,
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.boatPosition = boatPos;
+        this.image = loadImage("assets/boat.png");
+        World.add(world,this.body);
+
+    
+    }
+    display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,this.boatPosition,this.width,this.height);
+        noTint();
+        pop();
+    }
+}
+
